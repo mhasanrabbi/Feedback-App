09c86dae295cfe499b0c4a6f61a083322baad9da
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-05-30 22:19:15 +0600

    creating links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage';
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
         </Route>
         <Route path='/about' component={AboutPage} />
       </div>
+      <AboutIconLink />
     </Router>
   );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Card from '../shared/Card';
 
 function AboutPage() {
@@ -6,6 +7,10 @@ function AboutPage() {
       <div className='about'>
         <h1>About This Project</h1>
         <p>This is React app to leave feedback for a product or service</p>
+        <p>version: 1.0.0</p>
+        <p>
+          <Link to='/'>Back To Home</Link>
+        </p>
       </div>
     </Card>
   );
