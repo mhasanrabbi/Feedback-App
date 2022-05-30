08ca28b7da660a27bab85b19f4ba244213a529f7
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-05-30 21:22:45 +0600

    real-time validation

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Card from '../shared/Card';
+import Button from '../shared/Button';
+
+function FeedbackForm() {
+  const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+
+  const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
+    setText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* @todo - rating select component */}
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
