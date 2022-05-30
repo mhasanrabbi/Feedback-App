057631e2ed27c07caaab14a368602365addaaedd
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-05-30 20:55:28 +0600

    components, props and state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+import FeedbackList from './components/FeedbackList';
 import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className='container'>
-        <h1>Hello</h1>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,21 @@
+import { FaTimes } from 'react-icons/fa';
+import Card from '../shared/Card';
+import PropTypes from 'prop-types';
+
+function FeedbackItem({ item, handleDelete }) {
+  return (
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  );
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+export default FeedbackItem;
